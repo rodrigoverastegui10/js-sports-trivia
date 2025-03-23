5dcc2efd948340de66594da46d03a1bb81341cef
Rodrigo Verastegui <300095308@fvtc.edu>
2025-03-23 00:35:01 -0500

    Completed Exam 2

--- site.js
+++ site.js
@@ -11,12 +11,52 @@ const nextQuestionElement = document.querySelector('#nextQuestion')
 ;(async () => {
 
 	// todo: create your "getNextQuestion" function
-
+	const getNextQuestion = async newQuestion => {
+		const url = `https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple`;
+		const res = await fetch(url);
+		const json = await res.json();
+		
+		const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+	const answers = shuffle([ ...incorrect, correct ])
+	return { question, answers, correct }
+	}
+	
 	// todo: create your "renderQuestion" function
+	const renderQuestion = ({ question, answers, correct }) => {
+		questionElement.textContent = decodeHtml(question);
+		answersElement.innerHTML = '';
+		answers.forEach(answer =>{
+			const button = document.createElement('button');
+			button.textContent = decodeHtml(answer);
+			button.classList.add('answer');
 
+			button.addEventListener('click', async () => {
+				if (answer === correct) {
+					button.classList.add('correct')
+					answersElement.querySelectorAll('button').forEach(b => b.disabled = true)
+					alert('Correct!')
+					return;
+					renderQuestion(await getNextQuestion())
+					nextQuestionElement.disabled = true
+					setTimeout(() => nextQuestionElement.disabled = false, 10000)
+		
+				}
+				
+				button.disabled = true
+				alert('Incorrect!');
+			});
+			answersElement.appendChild(button);
+		});
+	};
 	// todo: add the event listener to the "nextQuestion" button
-
+	nextQuestionElement.addEventListener('click', async () =>{
+	renderQuestion(await getNextQuestion())
+	nextQuestionElement.disabled = true
+	setTimeout(() => nextQuestionElement.disabled = false, 10000)
+	
+	 });
 })()
 
 // mimic a click on the "nextQuestion" button to show the first question
-nextQuestionElement.click()
+nextQuestionElement.click() 
+	
